e8ccaaf8fdb77f581f8cca2822e4b37fd24fa349
chatgptdev <chatgptdev@tuta.io>
2023-04-09 10:50:10 +0200

    More secure implementation for secure_vector class

--- secure_vector.h
+++ secure_vector.h
@@ -22,38 +22,59 @@
 #include <Windows.h>
 #endif
 #include <vector>
-#include <algorithm>
+#include <memory>
 
+// Secure memory erasure macro for Windows and non-Windows systems
 #if defined(_WIN32)
 #define secure_zeromem(mem,size) do { RtlSecureZeroMemory (mem, size); } while (0)
 #else
 #define secure_zeromem(mem,size) do { volatile char *burnm = (volatile char *)(mem); int burnc = size; while (burnc--) *burnm++ = 0; } while (0)
 #endif
 
-// A secure_vector class that extends the standard std::vector class,
-// providing secure memory erasure for sensitive data.
-template <typename T, typename Allocator = std::allocator<T>>
-class secure_vector : public std::vector<T, Allocator> {
+
+// A custom allocator that extends the standard std::allocator<T> class,
+// providing secure memory erasure for sensitive data during deallocation.
+template <typename T>
+class secure_allocator : public std::allocator<T> {
 public:
-    using std::vector<T, Allocator>::vector; // Inherit all constructors from std::vector
+    using size_type = typename std::allocator<T>::size_type;
+    using pointer = typename std::allocator<T>::pointer;
+    using const_pointer = typename std::allocator<T>::const_pointer;
 
-    // Custom constructor for creating a secure_vector from two const_iterators
-    secure_vector(const_iterator first, const_iterator last)
-        : std::vector<T, Allocator>(first, last) {
+    template <typename U>
+    struct rebind {
+        using other = secure_allocator<U>;
+    };
+
+    secure_allocator() noexcept {}
+
+    secure_allocator(const secure_allocator& other) noexcept
+        : std::allocator<T>(other) {}
+
+    template <typename U>
+    secure_allocator(const secure_allocator<U>& other) noexcept
+        : std::allocator<T>(other) {}
+
+    ~secure_allocator() noexcept {}
+
+    pointer allocate(size_type n, const_pointer hint = 0) {
+        return std::allocator<T>::allocate(n, hint);
     }
 
-    // Custom destructor that securely erases the memory occupied by the vector
-    ~secure_vector() {
-        secure_clear();
-    }
-
-    // Securely erase the memory occupied by the vector and clear its contents
-    void secure_clear() {
-        auto& vec = static_cast<std::vector<T, Allocator>&>(*this);
-        if (!vec.empty()) {
-            secure_zeromem(vec.data(), vec.size() * sizeof(T));
-            vec.clear();
+    // Custom deallocate function that securely erases the memory occupied by the pointer
+    // before deallocating it using the base class deallocate function.
+    void deallocate(T* ptr, std::size_t n) {
+        if (ptr != nullptr && n > 0) {
+            secure_zeromem(ptr, n * sizeof(T));
         }
+        std::allocator<T>::deallocate(ptr, n);
     }
 };
+
+// A secure_vector class that is an alias for a std::vector using the secure_allocator.
+// This ensures that the memory occupied by sensitive data is securely erased during
+// deallocation, while also providing all the features and capabilities of a regular
+// std::vector.
+template <typename T>
+using secure_vector = std::vector<T, secure_allocator<T>>;
 
